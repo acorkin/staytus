795a2066d24d95b38ad52245a5050938745098a3
acorkin <aaron@Radioco-App.local>
2017-06-15 11:39:27 +0100

    Fix for 500 error on subscribe.

--- config/initializers/smtp_example.rb
+++ config/initializers/smtp_example.rb
@@ -1,14 +1,10 @@
 if ENV['STAYTUS_SMTP_HOSTNAME']
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    openssl_verify_mode: "none",
     :address    => ENV['STAYTUS_SMTP_HOSTNAME'],
     :port       => ENV['STAYTUS_SMTP_PORT'],
     :user_name  => ENV['STAYTUS_SMTP_USERNAME'],
-    :password   => ENV['STAYTUS_SMTP_PASSWORD'],
-    :enable_starttls_auto => ENV['STAYTUS_SMTP_STARTTLS'] == '1',
-    :domain                 => ENV['STAYTUS_SMTP_DOMAIN'],
-    :openssl_verify_mode   => ENV['STAYTUS_SMTP_VERIFY_SSL'] == OpenSSL::SSL::VERIFY_PEER
+    :password   => ENV['STAYTUS_SMTP_PASSWORD']
   }
 else
   puts "\e[33m=> You haven't configured an SMTP server. Mail will be sent using sendmail.\e[0m"
